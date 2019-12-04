cc3fdfff9b2d97d854bb79ffdd90667064f90e5c
Brian Perrett <perrettbrian@gmail.com>
2019-12-03 21:45:33 -0800

    day 4

--- day03/day3.py
+++ day03/day3.py
@@ -1,8 +1,9 @@
 """day3.py
 """
+import time
 
 def get_data():
-    with open('data.txt', 'r') as f:
+    with open('big.txt', 'r') as f:
         lines = [l.split(',') for l in f]
     return lines
 
@@ -10,23 +11,30 @@ def get_data():
 def get_all_coords(instructions):
     coord = (0, 0)
     all_coords = [tuple(coord)]
+    change = []
     d = ((0, 1), (1, 0), (0, -1), (-1, 0))
-    for instr in instructions:
+    for i, instr in enumerate(instructions):
         val = int(instr[1:])
         unit = d['URDL'.index(instr[0])]
         coord = (coord[0] + (unit[0]*val), coord[1] + (unit[1]*val))
         all_coords.append(coord)
-    return all_coords
+        if all_coords[i][0] == coord[0]:
+            change.append('x')
+        else:
+            change.append('y')
+    return all_coords, change
 
 
-def find_all_intersections(c_list_1, c_list_2):
+def find_all_intersections(c_list_1, change_1, c_list_2, change_2):
     """Get a list of intersecting points.
 
     Not considering if two lines are on top of eachother.
 
     """
     intersections = []
-    for i in range(0, len(c_list_1)-1):
+    list_1_len = len(c_list_1)
+    list_2_len = len(c_list_2)
+    for i in range(list_1_len-1):
         c1, c2 = c_list_1[i], c_list_1[i+1]
         cx_start, cx_end = c1[0], c2[0]
         big_cx = max([cx_start, cx_end])
@@ -36,20 +44,28 @@ def find_all_intersections(c_list_1, c_list_2):
         big_cy = max([cy_start, cy_end])
         small_cy = min([cy_start, cy_end])
         
-        if cx_start != cx_end:
-            c_change = 'x'
-        else:
-            c_change = 'y'
-        for j in range(0, len(c_list_2)-1):
-            d1, d2 = c_list_2[j], c_list_2[j+1]
+        c_change = 'x' if change_1[i] == 'y' else 'x'
+
+        for j in range(list_2_len-1):
+            loop = i * list_1_len + j
+            if loop % 100_000 == 0:
+                print(f'On loop {loop:,}')
+
+            if change_1[i] == change_2[j]:
+                continue
+
+            d1 = c_list_2[j]
+            d2 = c_list_2[j+1]
             
-            dx_start, dx_end = d1[0], d2[0]
-            big_dx = max([dx_start, dx_end])
-            small_dx = min([dx_start, dx_end])
+            dx_start = d1[0]
+            dx_end = d2[0]
+            big_dx = max((dx_start, dx_end))
+            small_dx = min((dx_start, dx_end))
             
-            dy_start, dy_end = d1[1], d2[1]
-            big_dy = max([dy_start, dy_end])
-            small_dy = min([dy_start, dy_end])
+            dy_start = d1[1]
+            dy_end = d2[1]
+            big_dy = max((dy_start, dy_end))
+            small_dy = min((dy_start, dy_end))
             
             if c_change == 'y':
                 if cx_start <= big_dx and cx_start >= small_dx:
@@ -65,7 +81,8 @@ def find_all_intersections(c_list_1, c_list_2):
                         intersections.append((dx_start, cy_start))
                 else:
                     continue
-    intersections.remove((0,0))
+    if (0, 0) in intersections:
+        intersections.remove((0,0))
     return intersections
 
 
@@ -107,13 +124,17 @@ def find_steps_to(inter, steps):
 
 if __name__ == '__main__':
     data = get_data()
+
+    start = time.time()
     c1, c2 = get_all_coords(data[0]), get_all_coords(data[1])
-    intersections = find_all_intersections(c1, c2)
+    intersections = find_all_intersections(c1[0], c1[1], c2[0], c2[1])
+    print(f'# intersections {len(intersections)}')
     closest_intersection = min(intersections, key=lambda elt: abs(elt[0]) + abs(elt[1]))
     print(f'Manhattan distance to closest intersection: {sum(map(abs, closest_intersection))}')
 
-    steps_to_intersections = [(find_steps_to(inter, c1), find_steps_to(inter, c2)) for inter in intersections]
-    print(steps_to_intersections)
+    steps_to_intersections = [(find_steps_to(inter, c1[0]), find_steps_to(inter, c2[0])) for inter in intersections]
     steps_each = min(steps_to_intersections, key=sum)
     shortest_wire_path = sum(steps_each)
     print(f'Shortest total wire path: {shortest_wire_path}')
+    end = time.time()
+    print(f'Total time: {end - start:0.2f}s')
---
+++ day04/day4.py
@@ -0,0 +1,52 @@
+"""day4.py
+"""
+
+VALID_RANGE = (245318, 765747)
+
+def is_valid(num, extra=True):
+    num_str = str(num)
+    if len(num_str) == 6:
+        if has_same_adj(num, extra=extra):
+            if num >= VALID_RANGE[0] and num <= VALID_RANGE[1]:
+                if is_increasing(num):
+                    return True
+    return False
+
+
+def has_same_adj(num, run_length=2, extra=True):
+    num_str = str(num)
+    counts = []
+    for c in num_str:
+        if len(counts) > 0 and counts[-1][0] == c:
+            counts[-1][1] += 1
+        else:
+            counts.append([c, 1])
+        if extra:
+            if counts[-1][1] >= 2:
+                return True
+    if not extra:
+        for elt in counts:
+            if elt[1] == 2:
+                return True
+    return False
+
+def is_increasing(num):
+    num_str = str(num)
+    for i in range(len(num_str) - 1):
+        if int(num_str[i]) > int(num_str[i+1]):
+            return False
+    return True
+
+def find_valid_count(extra=True):
+    count = 0
+    for i in range(VALID_RANGE[0], VALID_RANGE[1] + 1):
+        if is_valid(i, extra=extra):
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    valid_count = find_valid_count()
+    print(valid_count)
+
+    valid_count = find_valid_count(extra=False)
+    print(valid_count)
