f8d95e48ed9422f2860ec1cc1f9705d296d33bc5
Brian Perrett <perrettbrian@gmail.com>
2019-12-07 15:23:19 -0700

    use itertools.permutations

--- day07/day7.py
+++ day07/day7.py
@@ -1,35 +1,18 @@
 """day7.py
 """
 from computer import Computer
+from itertools import permutations
 
 
 def get_highest_signals(data, allowed, run_once=True):
     max_val = 0
     max_input = None
-    for signal in generate_allowed(allowed):
+    for signal in (''.join(p) for p in permutations(allowed)):
         output = run_with_signal(data, signal, run_once=run_once)
         if output > max_val:
             max_val = output
             max_input = signal
     return (max_input, max_val)
-
-
-def generate_allowed(allowed='01234'):
-    allowed = [x for x in allowed]
-    for n1 in allowed:
-        c1 = list(allowed)
-        c1.remove(n1)
-        for n2 in c1:
-            c2 = list(c1)
-            c2.remove(n2)
-            for n3 in c2:
-                c3 = list(c2)
-                c3.remove(n3)
-                for n4 in c3:
-                    c4 = list(c3)
-                    c4.remove(n4)
-                    signal = f'{n1}{n2}{n3}{n4}{c4[0]}'
-                    yield signal
 
 
 def run_with_signal(data, signal, run_once=True):
