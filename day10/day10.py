6012aef0a3e9376be16d90023c53ed73182494ab
Brian Perrett <perrettbrian@gmail.com>
2019-12-10 19:39:21 -0700

    slightly cleaner

--- day10/day10.py
+++ day10/day10.py
@@ -1,22 +1,20 @@
 """day10.py
 """
-from math import atan2
+from math import atan2, sqrt, pi
 
 def find_slope(start, end):
     num = end[1] - start[1]
     den = end[0] - start[0]
     slope = num / den if den != 0 else 'und'
-    top_sign = '+' if num>=0 else '-'
-    bot_sign = '+' if den>=0 else '-'
     angle = get_angle(start, end)
-    return slope, top_sign, bot_sign, angle
+    return slope, angle
 
 def get_angle(start, end):
     y = end[1] - start[1]
     x = end[0] - start[0]
     angle = atan2(x, -y)
     if angle < 0:
-        angle = (2*np.pi) + angle
+        angle = (2*pi) + angle
     return angle 
 
 def get_slopes_for(data, coord):
@@ -33,7 +31,7 @@ def get_slopes_for(data, coord):
 def get_distance(start, end):
     x = end[0] - start[0]
     y = end[1] - start[1]
-    return np.sqrt(x**2 + y**2)
+    return sqrt(x**2 + y**2)
 
 def count_all(data):
     counts = {}
@@ -47,30 +45,35 @@ def count_all(data):
 def most_visible_asteroids(data):
     counts = count_all(data)
     best = max(counts, key=lambda x: counts[x])
-    print(f'Best Location: {best}')
-    print(f'# Asteroids seen: {counts[best]}')
     return best, counts
+
+def get_vaporization_order(data):
+    asteroid_count = sum(line.count('#') for line in data)
+    slopes = get_slopes_for(data, best)
+    slopes = [(x[1], y) for x, y in slopes.items()]
+    slopes.sort(key=lambda x: x[0])
+
+    asteroids = [x[1] for x in slopes]
+    for points in asteroids:
+        points.sort(key=lambda x: x[1])
+
+    vaporized = []
+    while len(vaporized) < asteroid_count - 1:
+        for i, point_list in enumerate(asteroids):
+            if len(point_list) == 0:
+                continue
+            point = point_list.pop(0)
+            vaporized.append(point[0])
+    
+    return vaporized
 
 if __name__ == '__main__':
     with open('data.txt', 'r') as f:
         data = [line.strip() for line in f]
-        asteroid_count = sum(line.count('#') for line in data)
         
         best, counts = most_visible_asteroids(data)
+        print(f'Best Location: {best}')
+        print(f'# Asteroids seen: {counts[best]}')
 
-        slopes = get_slopes_for(data, best)
-        slopes = [(x[3], y) for x, y in slopes.items()]
-        slopes.sort(key=lambda x: x[0])
-
-        asteroids = [x[1] for x in slopes]
-        for points in asteroids:
-            points.sort(key=lambda x: x[1])
-
-        vaporized = []
-        while len(vaporized) < asteroid_count - 1:
-            for i, point_list in enumerate(asteroids):
-                if len(point_list) == 0:
-                    continue
-                point = point_list.pop(0)
-                vaporized.append(point[0])
-        print(vaporized[199])
+        vaporized = get_vaporization_order(data)
+        print(f'200th Vaporized: {vaporized[199]}')
